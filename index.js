17708c43ac88fc1d3cb2efb34f6e5a357954c064
Zhan1313 <zhan.k1313@gmail.com>
2023-03-13 23:04:15 +0600

    still working

--- index.js
+++ index.js
@@ -4,4 +4,15 @@ const player1Elem = wrapperElem.querySelector('.player1');
 const player2Elem = wrapperElem.querySelector('.player2');
 
 player1Elem.insertAdjacentHTML('beforeend', '<img src="KH.png" alt="KH"/>');
+player1Elem.insertAdjacentHTML('beforeend', '<img src="./assets/6C.png" alt="6C"/>');
+player1Elem.insertAdjacentHTML('beforeend', '<img src="./assets/6D.png" alt="6D"/>');
+player1Elem.insertAdjacentHTML('beforeend', '<img src="./assets/6H.png" alt="6H"/>');
+player1Elem.insertAdjacentHTML('beforeend', '<img src="./assets/6S.png" alt="6S"/>');
+player1Elem.insertAdjacentHTML('beforeend', '<img src="./assets/7S.png" alt="7S"/>');
+
+player2Elem.insertAdjacentHTML('beforeend', '<img src="./assets/7C.png" alt="7C"/>');
+player2Elem.insertAdjacentHTML('beforeend', '<img src="./assets/7D.png" alt="7D"/>');
+player2Elem.insertAdjacentHTML('beforeend', '<img src="./assets/7H.png" alt="7H"/>');
+player2Elem.insertAdjacentHTML('beforeend', '<img src="./assets/8H.png" alt="8H"/>');
+player2Elem.insertAdjacentHTML('beforeend', '<img src="./assets/8D.png" alt="8D"/>');
 player2Elem.insertAdjacentHTML('beforeend', '<img src="QD.jpg" alt="KH"/>');
