4300a473692c68e9aeb48a4efdbd95cc0009136f
Zhan1313 <zhan.k1313@gmail.com>
2023-03-15 18:19:09 +0600

    new commit

--- index.js
+++ index.js
@@ -49,6 +49,8 @@ let player2 = [];
 const wrapperElem = document.querySelector('.wrapper');
 const player1Elem = wrapperElem.querySelector('.player1');
 const player2Elem = wrapperElem.querySelector('.player2');
+const middleFieldElem = wrapperElem.querySelector('.middleField');
+const playFieldElem = middleFieldElem.querySelector('.playField');
 
 const dealCards = (someDeck, player1Deck, player2Deck) => {
     for (let i = 0; i < 6; i++) {
@@ -63,11 +65,24 @@ const twoPlayersDecks = dealCards(shuffledDeck, player1, player2);
 let play1Deck = twoPlayersDecks[0];
 let play2Deck = twoPlayersDecks[1];
 
+const oneCardOut = (chosenCard, player1Deck) => {
+    let lessCards = player1Deck.filter(card => card !== chosenCard);
+    dealRealCards(lessCards);
+}
+
 const dealRealCards = (player1Deck) => {
     for (let i = 0; i < player1Deck.length; i++) {
         player1Elem.insertAdjacentHTML('beforeend',
             `<img src="./assets/${player1Deck[i].level}${player1Deck[i].suit}.png" 
                     alt="${player1Deck[i].level}${player1Deck[i].suit}"/>`);
+        let pl1CardImgElem = player1Elem.querySelectorAll('img')[i];
+        pl1CardImgElem.addEventListener('click', (e) => {
+            let lessCards = player1Deck.filter(card => card !== player1Deck[i]);
+            console.log('this is less now', lessCards)
+            console.log(e.target)
+            pl1CardImgElem.remove();
+            playFieldElem.insertAdjacentHTML('afterbegin', player1Deck[i])
+        })
         player2Elem.insertAdjacentHTML('beforeend',
             `<img src="./assets/BackSideCard.png" 
                     alt="BackSideCard"/>`);
@@ -76,4 +91,13 @@ const dealRealCards = (player1Deck) => {
 
 dealRealCards(play1Deck);
 
-
+const useCard = () => {
+    const hitCard = (player1Deck) => {
+        let lessCards = player1Deck.filter(card => card !== player1Deck[i]);
+        console.log('this is less now', lessCards)
+        console.log(e.target)
+        pl1CardImgElem.remove();
+        hitCard(lessCards)
+    }
+    hitCard(player1Deck)
+}
