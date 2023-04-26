4e0ded2577c98cb11f052224735ce6dd41c22e1a
Zhan1313 <zhan.k1313@gmail.com>
2023-04-26 11:19:35 +0600

    new commit

--- main.js
+++ main.js
@@ -165,44 +165,17 @@ export const dealRealCards = (player1Deck, player2Deck, masterSuit, playField, f
 
             if (pl1CardImgElem === pl1CardImages.item(i)) {
                 player1Deck = actionOfPlayer1andPlayer2(pl1CardImgElem, playField, player2Deck, i, player1Deck);
+            } else if (pl1CardImgElem === pl1CardImages.item(i - 1)) {
+                player1Deck = actionOfPlayer1andPlayer2(pl1CardImgElem, playField, player2Deck, i - 1, player1Deck);
+            } else if (pl1CardImgElem === pl1CardImages.item(i - 2)) {
+                player1Deck = actionOfPlayer1andPlayer2(pl1CardImgElem, playField, player2Deck, i - 2, player1Deck);
+            } else if (pl1CardImgElem === pl1CardImages.item(i - 3)) {
+                player1Deck = actionOfPlayer1andPlayer2(pl1CardImgElem, playField, player2Deck, i - 3, player1Deck);
+            } else if (pl1CardImgElem === pl1CardImages.item(i - 4)) {
+                player1Deck = actionOfPlayer1andPlayer2(pl1CardImgElem, playField, player2Deck, i - 4, player1Deck);
+            } else if (pl1CardImgElem === pl1CardImages.item(i - 5)) {
+                player1Deck = actionOfPlayer1andPlayer2(pl1CardImgElem, playField, player2Deck, i - 5, player1Deck);
             }
-
-
-            setTimeout(() => {
-                for (let j = 0; j < player2Deck.length; j++) {
-                    let player2IsBigger = dummyPlay(player1AttackCard.level, player1AttackCard.suit,
-                        player2Deck[j].level, player2Deck[j].suit, masterSuit.suit);
-                    console.log('player2 wins?', player2IsBigger);
-                    console.log('====', j);
-
-                    if (player2IsBigger === true) {
-                        let player2DefenceCard = player2Deck.splice(j, 1)[0];
-                        console.log('this is updated player2 cards', player2Deck);
-                        console.log('this is defendCard of player2', player2DefenceCard);
-
-                        playField.push(player2DefenceCard);
-                        console.log('this is updated playField cards', playField);
-
-                        let pl2CardImgElem = player2Elem.querySelectorAll('img')[j];
-                        pl2CardImgElem.remove();
-                        playFieldElem.insertAdjacentHTML('afterbegin',
-                            `<img src="./assets/${player2DefenceCard.level}${player2DefenceCard.suit}.png" 
-                                    alt="${player2DefenceCard.level}${player2DefenceCard.suit}"/>`);
-                        return;
-                    }
-                }
-                playField.splice(0, 1);
-                console.log('this is updated cards on playField', playField);
-
-                let fieldPlay1ImgElem = playFieldElem.querySelector('img');
-                fieldPlay1ImgElem.remove();
-                player2Deck.push(player1AttackCard);
-                console.log('this is updated player2 cards', player2Deck);
-
-                player2Elem.insertAdjacentHTML('beforeend',
-                    `<img src="./assets/${player1AttackCard.level}${player1AttackCard.suit}.png" 
-                            alt="${player1AttackCard.level}${player1AttackCard.suit}"/>`);
-            }, 2000);
         })
         player2Elem.insertAdjacentHTML('beforeend',
             `<img src="./assets/${player2Deck[i].level}${player2Deck[i].suit}.png" 
