f4ac37494299453de31ee961d7b9b52dad6cd15a
Zhan1313 <zhan.k1313@gmail.com>
2023-05-02 21:49:57 +0600

    new commit

--- comparison.js
+++ comparison.js
@@ -7,7 +7,7 @@ export const dummyPlay = (Card1_Level, Card1_Suit, Card2_Level, Card2_Suit, Supe
                 if(Card2_Level === levels[j]) {
                     if (j > i && Card1_Suit === Card2_Suit) {
                         return true;
-                    } else if ((j <= i && Card2_Suit === SuperSuit) && Card1_Suit !== SuperSuit) {
+                    } else if (Card2_Suit === SuperSuit && Card1_Suit !== SuperSuit) {
                         return true;
                     }
                     return false;
